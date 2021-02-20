665a848435bdab49c480ef5c9b8dc7c1e205dff3
Derik-son <taras.timoshuk93@gmail.com>
2021-02-20 11:39:50 +0200

    fix

--- monticello.js
+++ monticello.js
@@ -3,7 +3,20 @@ $('.slider').slick({
    slidesToShow: 3,
    slidesToScroll: 1,
    dots:true,
-   
-   
-   
    });
+
+   $('.header-slider').slick({
+      lazyLoad:'progressive',
+      draggable: false,
+      infinite: true,
+      
+      arrows: false,
+      autoplay: true,
+      speed: 10,
+   });
+   $('#header-slider').on('afterChange', function(_event,slick,currentSlide){
+      if (currentSlide ==3) { 
+        console.log('yes, there will be all title'); 
+      }
+    });
+    
